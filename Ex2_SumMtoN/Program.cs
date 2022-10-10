a26c198d4816a9b8356b0a0497a28ab93ffb2c68
Eric <ericismag@outlook.com>
2022-10-11 02:04:21 +0300

    Second Program Sum

---
+++ Ex2_SumMtoN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задать значения M и N. Написать программу, которая найдет
+// сумму натуральных чисел в промежутке от M до N.
+
+Console.Clear();
+int sum = 0;
+int SumForMtoN (int M, int N)
+{
+    if (M > N) return sum;
+    else    return sum + M + SumForMtoN(M+1,N); 
+}
+
+Console.WriteLine("Введите числа M и N:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int numbetN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumForMtoN(numberM, numbetN));
+
