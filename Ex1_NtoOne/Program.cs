fb48d12b7664508a47c45ef291a97200fc01bfb0
Eric <ericismag@outlook.com>
2022-10-11 01:51:38 +0300

    First Program N to One

---
+++ Ex1_NtoOne/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать значение N. НАписать программу, которая выведет
+// все натуральные числа в промежутке от N до 1.
+
+Console.Clear();
+
+Console.WriteLine("Введите число N:");
+int myNumber = Convert.ToInt32(Console.ReadLine());
+string lineIntNum = String.Empty;
+
+void PrintNumbers (int num)
+{
+    if (num == 0) Console.WriteLine (lineIntNum);
+    else
+    {
+        lineIntNum = lineIntNum + " " + Convert.ToString(num);
+        PrintNumbers(num -1);
+    }
+}
+
+PrintNumbers(myNumber);
+
